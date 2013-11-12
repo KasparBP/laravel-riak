9ef0ed0b3e9821b9cf755fe4bff5e6f0d0fa23da
Kaspar Pedersen <kaspar@inka-apps.com>
2013-11-12 19:20:28 +0100

    Initial commit

---
+++ src/BachPedersen/LaravelRiak/RiakServiceProvider.php
@@ -0,0 +1,34 @@
+<?php namespace BachPedersen\LaravelRiak;
+
+use Illuminate\Support\ServiceProvider;
+
+class RiakServiceProvider extends ServiceProvider {
+
+	/**
+	 * Indicates if loading of the provider is deferred.
+	 *
+	 * @var bool
+	 */
+	protected $defer = true;
+
+	/**
+	 * Register the service provider.
+	 *
+	 * @return void
+	 */
+	public function register()
+	{
+		//
+	}
+
+	/**
+	 * Get the services provided by the provider.
+	 *
+	 * @return array
+	 */
+	public function provides()
+	{
+		return array();
+	}
+
+}
